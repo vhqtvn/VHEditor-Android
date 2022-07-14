5cd7318f4c0df7994b3c8efab85ee32b26a1162f
vhqtvn <vhqtvn@users.noreply.github.com>
2022-07-14 13:15:19 +0700

    nope

--- release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VERSION=$(cat app/build.gradle | grep versionName | awk '{print $2}' | sed -e 's/^"//' -e 's/"$//')
-if [ $(git tag -l "v$VERSION") ]; then
-  echo "Version already exists"
-else
-  git tag -a v$VERSION -m "Version v$VERSION"
-  echo "Created tag $VERSION, push --tags now"
-fi
